b91e7ef45d02e453bef39382fec315f57e113cc5
gunadevan <gunadevandesing@gmail.com>
2023-12-15 18:08:52 +0530

    Homepage changes

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import KnowAboutUSSection from "../../components/HomePageComponents/KnowAboutUsSection";
+import VideoSection from "../../components/HomePageComponents/VideoSection";
+import "./homepage.scss";
+
+const HomePage = () => {
+  return (
+    <>
+      <VideoSection />
+      <KnowAboutUSSection />
+    </>
+  );
+};
+
+export default HomePage;
