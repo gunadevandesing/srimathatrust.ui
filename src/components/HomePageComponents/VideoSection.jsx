1d8f8e9263a15c0bc3b8b754d1ae7308bed857b5
gunadevan <gunadevandesing@gmail.com>
2023-12-24 23:33:21 +0530

    Updated homepage

--- src/components/HomePageComponents/VideoSection.jsx
+++ src/components/HomePageComponents/VideoSection.jsx
@@ -1,7 +1,30 @@
 import "./videoSection.scss";
+import PlayIcon from "../../assets/svg-icons/play-icon.svg";
 
 const VideoSection = () => {
-  return <div className="homepage-video"></div>;
+  return (
+    <div className="homepage-container">
+      <div className="homepage-content">
+        <div className="video-header">
+          Inclusive care for cancer patients with special needs
+        </div>
+        <div className="button-section">
+          <button className="white-button" onClick={() => {}}>
+            What we do
+          </button>
+          <div className="playvideo-button">
+            <img src={PlayIcon} alt="Play" />
+            Play Video
+          </div>
+        </div>
+        <div className="bottom-textsection">
+          <span className="text">230 people under our care</span>
+          <span className="horizontal-line"></span>
+          <span className="text">58 donations collected</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoSection;
