4bc54132ddee27c6e6d01943ca7a45322bf78a12
gunadevan-1 <gunadevan.desing@outlook.com>
2023-12-27 18:09:10 +0530

    Media page added

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,7 @@ const AboutUsPage = lazy(() => import("./pages/AboutUsPage/AboutUsPage.jsx"));
 const DonationPage = lazy(() =>
   import("./pages/DonationPage/DonationPage.jsx")
 );
+const MediaPage = lazy(() => import("./pages/MediaPage/MediaPage.jsx"));
 
 const RoutesComponent = () => {
   return (
@@ -13,6 +14,7 @@ const RoutesComponent = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="aboutus" element={<AboutUsPage />} />
       <Route path="donation" element={<DonationPage />} />
+      <Route path="media" element={<MediaPage />} />
       <Route path="*" element={<>Not found</>} />
     </Routes>
   );
